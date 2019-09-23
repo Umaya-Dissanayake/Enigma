f84f1f20e49773723d28d24c600d190ba42726fb
Samitha-Vidhanaarachchi <samithavidhanaarachchi@gmail.com>
2019-09-23 15:28:13 +0530

    admin login

--- app/src/main/java/com/example/enigma/AdminLogin.java
+++ app/src/main/java/com/example/enigma/AdminLogin.java
@@ -11,37 +11,5 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-
-
-public class AdminLogin extends Fragment {
-
-
-
-
-//    @Override
-//    public void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//
-//        }
-//
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View view =inflater.inflate(R.layout.fragment_admin_login, container, false);
-        Button btnadminfr = (Button)view.findViewById(R.id.btn_loginadmin);
-
-        btnadminfr.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                FragmentTransaction ft = getFragmentManager().beginTransaction();
-                ft.replace(R.id.screen_area,new panel());
-                ft.commit();
-
-            }
-        });
-        return view;
-    }
-
-}
-
+import android.widget.EditText;
+import android.widget.Toast;
